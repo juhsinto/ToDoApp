459f928c987b7c4177af0152eaa45c3257848f50
Jacinto Mendes <jacinto.mendes@outlook.com>
2017-04-18 12:24:54 +1000

    all done; menu fixed; pages updated

--- src/app/pages/about.component.ts
+++ src/app/pages/about.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     <h1> About </h1>
     <p> This app is made as a demo, to show off my skills in Angular!</p>
     <p> This app uses The <a href="http://getbootstrap.com/">Bootstrap</a> CSS framework, <a href="https://angular.io/">AngularJS (2)</a>, and <a href="https://firebase.google.com/">Firebase</a> </p>
-
+    <p> To see the source of this app, please visit <a href="https://github.com/juhsinto/ToDoApp">this</a> link.</p>
   `
 })
 
