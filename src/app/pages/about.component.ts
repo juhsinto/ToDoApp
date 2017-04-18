16afff1a594a94a34e82c4e247b95d00ce502f5f
Jacinto Mendes <jacinto.mendes@outlook.com>
2017-04-18 12:23:45 +1000

    all done; menu fixed; pages updated

--- src/app/pages/about.component.ts
+++ src/app/pages/about.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'about',
   template: `
-    <h1> about </h1>
+    <h1> About </h1>
+    <p> This app is made as a demo, to show off my skills in Angular!</p>
+    <p> This app uses The <a href="http://getbootstrap.com/">Bootstrap</a> CSS framework, <a href="https://angular.io/">AngularJS (2)</a>, and <a href="https://firebase.google.com/">Firebase</a> </p>
+
   `
 })
 
--- src/app/pages/help.component.ts
+++ src/app/pages/help.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'help',
   template: `
-    <h1> help </h1>
+    <h1> Help </h1>
+    <p> This app is simple to use, simply add your todo item, and click 'add' </p>
+    <p> If you want to delete an item, click the circular 'x' button at the side of the todo item </p>
   `,
   styleUrls: ['../app.component.css']
 })
